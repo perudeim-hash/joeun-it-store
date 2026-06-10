a1d99f7007df3b0c324a4f587d049f0ce58b898f
sejin <perudeim@gmail.com>
2026-06-10 15:51:42 +0900

    26/06/10 공통 파일 commit

---
+++ src/main/java/org/store/joeunit/JoeunItStoreApplication.java
@@ -0,0 +1,13 @@
+package org.store.joeunit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoeunItStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoeunItStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/store/joeunit/JoeunItStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package org.store.joeunit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoeunItStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
