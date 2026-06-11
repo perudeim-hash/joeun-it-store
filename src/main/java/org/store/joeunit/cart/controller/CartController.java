7cce776d1a165598a8deae5d4f8e3ad87f554873
sejin <perudeim@gmail.com>
2026-06-11 17:22:22 +0900

    26/06/11 CartController(Item Update-Ajax 구현) 및 carts Html 구현 , main화면 구현

--- src/main/java/org/store/joeunit/cart/controller/CartController.java
+++ src/main/java/org/store/joeunit/cart/controller/CartController.java
@@ -3,14 +3,13 @@ package org.store.joeunit.cart.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.store.joeunit.cart.dto.CartItemDto;
 import org.store.joeunit.cart.service.CartService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/cart")
 @Controller
@@ -32,16 +31,52 @@ public class CartController {
         return "cart/carts";
     }
 
+    @PostMapping("/update")
+    public String cartItemUpdate( CartItemDto cartItemDto) {
+        int memberId = 2;
+        cartItemDto.setMemberId(memberId);
+        cartService.updateCartItem(cartItemDto);
+        return "redirect:/cart/carts";
+    }
+
+    @PostMapping("/update-ajax")
+    @ResponseBody
+    public Map<String, Object> cartItemUpdateAjax(CartItemDto cartItemDto) {
+
+        int memberId = 2;
+        cartItemDto.setMemberId(memberId);
+
+        int result = cartService.updateCartItem(cartItemDto);
+        int cartTotalPrice = cartService.getCartTotalPrice(memberId);
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("success", result > 0);
+        response.put("cartItemId", cartItemDto.getCartItemId());
+        response.put("quantity", cartItemDto.getQuantity());
+        response.put("cartTotalPrice", cartTotalPrice);
+
+        return response;
+    }
+
+    @PostMapping("/add")
+    public String cartItemAdd(CartItemDto cartItemDto) {
+        int memberId = 2;
+        cartItemDto.setMemberId(memberId);
+        cartService.addCartItem(cartItemDto);
+        return "redirect:/cart/carts";
+    }
+
+
     // 아이텝 삭제
     @PostMapping("/delete")
-    public String CartItemDelete(int cartItemId) {
+    public String cartItemDelete(@RequestParam int cartItemId) {
         int memberId = 2;
-        cartService.deleteCartItem(memberId,cartItemId);
+        cartService.deleteCartItem(memberId, cartItemId);
         return "redirect:/cart/carts";
     }
     //장바구니 전체 삭제
     @PostMapping("/delete-all")
-    public String CartItemDeleteAll() {
+    public String cartItemDeleteAll() {
         int memberId = 2;
         cartService.deleteAllCartItem(memberId);
         return "redirect:/cart/carts";
