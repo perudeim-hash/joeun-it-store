b183f6af1dc1f57c3304d7dbee844f655dcc6134
leesanggak <wiinball@gmail.com>
2026-06-15 14:26:15 +0900

    2026-06-15 product modify/write js생성 및 html수정

---
+++ src/main/resources/static/js/product/write.js
@@ -0,0 +1,23 @@
+function previewImage(input){
+
+    const file = input.files[0];
+
+    if(!file){
+        return;
+    }
+
+    const reader = new FileReader();
+
+    reader.onload = function(e){
+
+        const preview =
+            document.querySelector("#preview");
+
+        preview.src =
+            e.target.result;
+
+        preview.classList.remove("hidden");
+    };
+
+    reader.readAsDataURL(file);
+}
