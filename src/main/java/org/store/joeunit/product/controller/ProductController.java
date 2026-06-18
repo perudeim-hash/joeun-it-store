3ea6cf49388c25c155e6f769d678d21f83c2dca4
sejin <perudeim@gmail.com>
2026-06-18 17:38:43 +0900

    26/06/17 cart 버그 수정

--- src/main/java/org/store/joeunit/product/controller/ProductController.java
+++ src/main/java/org/store/joeunit/product/controller/ProductController.java
@@ -118,7 +118,7 @@ public class ProductController {
                             productId
                     );
 
-            canWriteReview = (isAdmin || hasPurchased) && !hasReviewed;
+            canWriteReview = hasPurchased && !hasReviewed;
         }
 
         model.addAttribute("canWriteReview", canWriteReview);
@@ -186,15 +186,8 @@ public class ProductController {
             productDto.setImageName(savedFileName);
             productDto.setImagePath("/uploads/" + savedFileName);
         }
-        if(productDto.getCategoryId() == 4){
 
-            productDto.setCpu(null);
-            productDto.setRam(null);
-            productDto.setStorageCapacity(null);
-            productDto.setOs(null);
-            productDto.setScreenSize(null);
-
-        if(productDto.getCategoryId() == 4){
+        if (productDto.getCategoryId() == 4) {
 
             productDto.setCpu(null);
             productDto.setRam(null);
@@ -299,20 +292,5 @@ public class ProductController {
         result.put("totalPage", totalPage);
 
         return result;
-    }
-    @GetMapping("/product/search")
-    public String search(@RequestParam String keyword,
-        Model model) {
-
-    List<ProductDto> productList =
-            productService.search(keyword);
-
-    model.addAttribute("productList",productList);
-    model.addAttribute("keyword",keyword);
-    model.addAttribute("currentPage",1);
-    model.addAttribute("totalPage",1);
-    model.addAttribute("categoryId",0);
-
-    return "product/list";
     }
 }
